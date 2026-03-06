3752835e83dfb3b564de417505495f35b013e56f
KDDilshan <kdprojects847@gmail.com>
2026-03-06 17:58:53 +0530

    refactor: Update FeignClient name to WORKFORCE-EQUIPMENT-SERVICE in WorkerClient

--- backend/PickingPackingService/src/main/java/com/wms/picking_packing_service/client/WorkerClient.java
+++ backend/PickingPackingService/src/main/java/com/wms/picking_packing_service/client/WorkerClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "${services.worker.name:CUSTOMER-MANAGEMENT-SERVICE}")
+@FeignClient(name = "${services.worker.name:WORKFORCE-EQUIPMENT-SERVICE}")
 public interface WorkerClient {
 
     @GetMapping("/api/v1/workers/{workerId}")
